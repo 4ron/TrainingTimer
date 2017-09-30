fa4771ecb177156b7372e32dcb4cd2d9e58a48ac
aron <aron@atfinity.ch>
2017-09-30 17:15:15 +0200

    added UI elements and a button that leads us to another activity

--- app/src/main/java/com/trainer/org/trainingtimer/CounterActivity.java
+++ app/src/main/java/com/trainer/org/trainingtimer/CounterActivity.java
@@ -1,15 +1,15 @@
 package com.trainer.org.trainingtimer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
-public class Counter extends AppCompatActivity {
+public class CounterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +26,14 @@ public class Counter extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Get the intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String period = intent.getStringExtra(MainActivity.TRAINER_PERIOD);
+
+        // Add the string to a text view in the layout
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText("Starting countdown from " + period + "...");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_counter, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
