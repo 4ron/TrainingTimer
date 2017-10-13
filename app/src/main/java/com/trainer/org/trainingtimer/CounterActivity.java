47ebd216940657c722ce951ebe116b31c44afdec
aron <aron@atfinity.ch>
2017-10-13 22:20:06 +0200

    added simple countdown timer

--- app/src/main/java/com/trainer/org/trainingtimer/CounterActivity.java
+++ app/src/main/java/com/trainer/org/trainingtimer/CounterActivity.java
@@ -2,17 +2,25 @@ package com.trainer.org.trainingtimer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class CounterActivity extends AppCompatActivity {
 
+    CountDownTimer timer;
+    TextView statusText;
+    TextView countdown;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -27,13 +35,34 @@ public class CounterActivity extends AppCompatActivity {
             }
         });
 
+        // the UI elements
+        statusText = (TextView) findViewById(R.id.textView2);
+        countdown = (TextView) findViewById(R.id.textView3);
+
         // Get the intent that started this activity and extract the string
         Intent intent = getIntent();
         String period = intent.getStringExtra(MainActivity.TRAINER_PERIOD);
 
-        // Add the string to a text view in the layout
-        TextView textView = (TextView) findViewById(R.id.textView2);
-        textView.setText("Starting countdown from " + period + "...");
+        // start the timer
+        setupCountDownTimer(Integer.parseInt(period)*1000, 1000);
+        statusText.setText("Work!");
+        timer.start();
     }
 
+    private void setupCountDownTimer(long millisInFuture, long countDownInterval) {
+        timer = new CountDownTimer(millisInFuture, countDownInterval) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.d("app", "timer > onTick()");
+                countdown.setText("" + (millisUntilFinished / 1000 + 1));
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("app", "timer > onFinish()");
+                countdown.setText(0 + "");
+                statusText.setText("DONE!");
+            }
+        };
+    }
 }
